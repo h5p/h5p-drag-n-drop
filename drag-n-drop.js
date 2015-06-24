44fac60f732029b898123a222b6dec041eac262d
Frode Petterson <frode.andre.petterson@gmail.com>
2015-06-24 15:32:21 +0200

    No more threshold, always move.

--- drag-n-drop.js
+++ drag-n-drop.js
@@ -7,12 +7,9 @@ var H5P = H5P || {};
  * @returns {undefined}
  */
 H5P.DragNDrop = function ($container) {
-  this.moveThreshold = 4;
   this.$container = $container;
   this.scrollLeft = 0;
   this.scrollTop = 0;
-  
-  // TODO: Use jQuery events instead of callbacks?
 };
 
 /**
@@ -35,7 +32,7 @@ H5P.DragNDrop.prototype.press = function ($element, x, y) {
     // With user-select: none uncommented, after moving a drag and drop element, if I hover over something that changes transparancy on hover IE10 on WIN7 crashes
     // TODO: Add user-select and -ms-user-select later if IE10 stops bugging
     .css({'-moz-user-select': 'none', '-webkit-user-select': 'none'/*, 'user-select': 'none', '-ms-user-select': 'none'*/})
-    .mousemove(eventData, H5P.DragNDrop.move)
+    .mousemove(eventData, H5P.DragNDrop.moveHandler)
     .attr('unselectable', 'on')[0]
     .onselectstart = H5P.$body[0].ondragstart = function () {
       return false;
@@ -56,39 +53,45 @@ H5P.DragNDrop.prototype.press = function ($element, x, y) {
     x: x - offset.left + this.marginX,
     y: y - offset.top - this.marginY
   };
+
+  this.move(x, y);
+};
+
+/**
+ * Handles mouse move events.
+ *
+ * @param {Event} event
+ */
+H5P.DragNDrop.moveHandler = function (event) {
+  event.stopPropagation();
+  event.data.instance.move(event.pageX, event.pageY);
 };
 
 /**
  * Handles mouse movements.
  *
- * @param {Object} event
- * @returns {undefined}
+ * @param {number} x
+ * @param {number} y
  */
-H5P.DragNDrop.move = function (event) {
-  event.stopPropagation();
-  var that = event.data.instance;
+H5P.DragNDrop.prototype.move = function (x, y) {
+  var that = this;
 
   if (!that.moving) {
-    if (event.pageX > that.startX + that.moveThreshold || event.pageX < that.startX - that.moveThreshold || event.pageY > that.startY + that.moveThreshold || event.pageY < that.startY - that.moveThreshold) {
-      if (that.startMovingCallback !== undefined && !that.startMovingCallback(event)) {
-        return;
-      }
-
-      // Start moving
-      that.moving = true;
-      that.$element.addClass('h5p-moving');
-    }
-    else {
+    if (that.startMovingCallback !== undefined && !that.startMovingCallback(x, y)) {
       return;
     }
+
+    // Start moving
+    that.moving = true;
+    that.$element.addClass('h5p-moving');
   }
 
-  var x = event.pageX - that.adjust.x;
-  var y = event.pageY - that.adjust.y;
+  x -= that.adjust.x;
+  y -= that.adjust.y;
 
   var posX = x - that.containerOffset.left + that.scrollLeft;
   var posY = y - that.containerOffset.top + that.scrollTop;
-  
+
 
   if (that.snap !== undefined) {
     posX = Math.round(posX / that.snap) * that.snap;
@@ -112,7 +115,7 @@ H5P.DragNDrop.release = function (event) {
   var that = event.data.instance;
 
   H5P.$body
-    .unbind('mousemove', H5P.DragNDrop.move)
+    .unbind('mousemove', H5P.DragNDrop.moveHandler)
     .unbind('mouseup', H5P.DragNDrop.release)
     .unbind('mouseleave', H5P.DragNDrop.release)
     .css({'-moz-user-select': '', '-webkit-user-select': ''/*, 'user-select': '', '-ms-user-select': ''*/})
@@ -127,6 +130,6 @@ H5P.DragNDrop.release = function (event) {
     that.$element.removeClass('h5p-moving');
     if (that.stopMovingCallback !== undefined) {
       that.stopMovingCallback(event);
-    };
+    }
   }
 };
